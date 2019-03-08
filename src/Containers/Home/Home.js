d5b47c846fd5e846994c89f40792fbb6d076b302
RisingGeek <bhavaypuri15@gmail.com>
2019-03-08 17:23:31 +0530

    added project structure

---
+++ src/Containers/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Navbar from '../../Components/Navbar/NavbarComponent';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+            </div>
+        );
+    }
+}
+
+export default Home;
